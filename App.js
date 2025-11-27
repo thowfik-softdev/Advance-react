8b4ca7ed2e9d62ec6301b40a313b7c3ed27cd8dc
Thowfik Juhair <thowfik.softdev@gmail.com>
2025-11-27 10:32:45 +0530

    jsx babel conversion

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ const heading = React.createElement("h1", { id: "heading" }, "Hello React!");
 console.log(heading); // return object
 
 //JSX - HTML like or XML like syntax, JSX is not html inside js
+//JS - Transiled before it reaches the JS engine
 const jsxHeading = <h1 id="heading">Hello from JSX!</h1>;
 console.log(jsxHeading); // return object
 
